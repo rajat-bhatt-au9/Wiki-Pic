407c2059367ddcbcd96aa5ed156f396987b84e54
rajat-bhatt-au9 <rajatbhatt500@gmail.com>
2021-02-07 22:46:51 +0530

    project completed

--- wiki-pic/src/components/search/Search.js
+++ wiki-pic/src/components/search/Search.js
@@ -1,37 +1,54 @@
-import React from "react";
+import React, { Component } from "react";
 import TextField from "material-ui/TextField";
 import SelectField from "material-ui/SelectField";
-import { MenuItem } from "material-ui";
+import MenuItem from "material-ui/MenuItem";
+import axios from "axios";
+import ImageResults from "../image-results/ImageResults";
 
-class Search extends React.Component {
+class Search extends Component {
   state = {
     searchText: "",
     amount: 15,
-    apiUrl: "https://pixabay.com/api/",
-    key: "20178801-8068d6e491faf4b9d023d3112",
+    apiUrl: "https://pixabay.com/api",
+    apiKey: "20178801-8068d6e491faf4b9d023d3112",
     images: [],
   };
 
   onTextChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
+    const val = e.target.value;
+    this.setState({ [e.target.name]: val }, () => {
+      if (val === "") {
+        this.setState({ images: [] });
+      } else {
+        axios
+          .get(
+            `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true`
+          )
+          .then((res) => this.setState({ images: res.data.hits }))
+          .catch((err) => console.log(err));
+      }
+    });
   };
 
+  onAmountChange = (e, index, value) => this.setState({ amount: value });
+
   render() {
+    console.log(this.state.images);
     return (
       <div>
         <TextField
           name="searchText"
           value={this.state.searchText}
           onChange={this.onTextChange}
-          floatingLabelText="Search for Images"
-          fillWidth={true}
+          floatingLabelText="Search For Images"
+          fullWidth={true}
         />
         <br />
         <SelectField
           name="amount"
+          floatingLabelText="Amount"
           value={this.state.amount}
           onChange={this.onAmountChange}
-          floatingLabelText="Amount"
         >
           <MenuItem value={5} primaryText="5" />
           <MenuItem value={10} primaryText="10" />
@@ -39,8 +56,10 @@ class Search extends React.Component {
           <MenuItem value={30} primaryText="30" />
           <MenuItem value={50} primaryText="50" />
         </SelectField>
-
         <br />
+        {this.state.images.length > 0 ? (
+          <ImageResults images={this.state.images} />
+        ) : null}
       </div>
     );
   }
